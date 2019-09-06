418581ff10cf781fec6cde8899d59ad8e804b7cc
Kalle Lehtonen <coadiespla@gmail.com>
2019-09-06 10:05:08 +0300

    Removed prod & dev apiUris, just use prod

--- src/app/services/koodisto-proxy.service.ts
+++ src/app/services/koodisto-proxy.service.ts
@@ -8,9 +8,7 @@ import { environment } from '../../environments/environment';
   providedIn: 'root'
 })
 export class KoodistoProxyService {
-  private apiUri: string;
-  private apiUriProd = 'https://koodisto.aoe.fi/api/v1';
-  private apiUriDev = 'http://localhost:3000/api/v1';
+  private apiUri: 'https://koodisto.aoe.fi/api/v1';
 
   private httpOptions = {
     headers: new HttpHeaders({
@@ -19,9 +17,7 @@ export class KoodistoProxyService {
     }),
   };
 
-  constructor(private http: HttpClient) {
-    this.apiUri = environment.production ? this.apiUriProd : this.apiUriDev;
-  }
+  constructor(private http: HttpClient) {}
 
   getData(rediskey: string, lang: string): Observable<any> {
     return this.http.get(`${this.apiUri}/${rediskey}/${lang}`, this.httpOptions);
